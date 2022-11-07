0c3f92153853016e69058cb90515dea98c7ba57a
Juhwan Kim <juhwan.boxcat@gmail.com>
2022-11-07 12:03:15 +0900

    Unity Inspector에서 class의 attribute를 변경할 수 있도록 Car class 수정

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -1,12 +1,32 @@
 using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
-
+[System.Serializable]
 public class Car
 {
     public float speed;
     public string color;
     public int highestSpeed;
+
+    // Constructor of the class
+    public Car() 
+    {
+        Debug.Log("Car() called");
+    }
+
+    public Car(float speed)
+    {
+        this.speed = speed;
+        Debug.Log("Car(float) is called");
+    }
+
+    public Car(float speed, string color)
+    {
+        this.speed = speed;
+        this.color = color;
+        Debug.Log("Car(float,string) is called");
+    }
     
+    // Methods
     void Move()
     {
         Debug.Log("Moving");
@@ -20,5 +40,12 @@ public class Car
     public void CarSpeed()
     {
         Debug.Log(speed);
+    }
+
+    public void PrintCarDetails()
+    {
+        Debug.Log("Speed = " + speed);
+        Debug.Log("Color = " + color);
+        Debug.Log("Highest Speed = " + highestSpeed);
     }
 }
--- Assets/Scripts/ClassTest.cs
+++ Assets/Scripts/ClassTest.cs
@@ -4,21 +4,28 @@ using UnityEngine;
 
 public class ClassTest : MonoBehaviour
 {
+
+    public Car myCar;
+    
     // Start is called before the first frame update
     void Start()
     {
-        Car myCar = new Car();
-        myCar.speed = 10.5f;
-        myCar.color = "Red";
-        myCar.highestSpeed = 100;
-
-        Car suzuki = new Car();
-        suzuki.speed = 20.0f;
-        suzuki.color = "Green";
-        suzuki.highestSpeed = 150;
-
-        myCar.CarSpeed();
-        suzuki.CarSpeed();
+        myCar.PrintCarDetails();
+        // Car myCar = new Car();
+        // myCar.speed = 10.5f;
+        // myCar.color = "Red";
+        // myCar.highestSpeed = 100;
+        //
+        // Car suzuki = new Car();
+        // suzuki.speed = 20.0f;
+        // suzuki.color = "Green";
+        // suzuki.highestSpeed = 150;
+        //
+        // myCar.CarSpeed();
+        // suzuki.CarSpeed();
+        //
+        // Car myCar2 = new Car(2.5f);
+        // Car myCar3 = new Car(3.0f, "Blue");
     }
 
     // Update is called once per frame
